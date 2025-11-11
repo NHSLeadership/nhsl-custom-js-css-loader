bedf3b8e13c782d66aaa9b3498a63eafa5f51360
Tawmu <Tawmu@users.noreply.github.com>
2025-11-11 09:32:01 +0000

    bump version to 1.1

--- nhsl-custom-js-css-loader.php
+++ nhsl-custom-js-css-loader.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: NHSL Custom JS & CSS Loader
  * Description: A simple plugin to load custom JavaScript and CSS.
- * Version: 1.0
+ * Version: 1.1
  * Author: Chris Witham
  *
  * @package nhsl-custom-js-css-loader
